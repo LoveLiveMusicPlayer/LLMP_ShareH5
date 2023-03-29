c3574b19b2a81884a08b990784da6a0e4491bc61
zhushenwudi <55681140@163.com>
2023-03-29 13:43:28 +0800

    fix: android修改最新apk

--- src/utils/appUtil.ts
+++ src/utils/appUtil.ts
@@ -3,6 +3,6 @@ const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 export default {
     jumpToDownload: async (isAndroid: boolean, millisecond?: number) => {
         await delay(millisecond ?? 0)
-        return isAndroid ? 'https://hey.scandown.com/qp49' : 'itms-apps://itunes.apple.com/app/1641625393'
+        return isAndroid ? 'https://picbed-cdn.zhushenwudi.top/llmp-oss/LLMP-M/version/64bit/1.2.5.apk' : 'itms-apps://itunes.apple.com/app/1641625393'
     }
 }
